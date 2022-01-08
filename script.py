6c57f7e850d57f0af8ba2a47c2a0a18d7c56f090
SDBUB7 <94208123+SDBUB7@users.noreply.github.com>
2022-01-08 20:02:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
+I'm an booksClubFranchise filter bot with many capabilities!
+I am Working Only BCF.
+Powered By: @BooksClubFranchise
 
 See <i>/help</i> for commands and more details.</b>
 """
@@ -54,10 +55,11 @@ See <i>/help</i> for commands and more details.</b>
 
     ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @SDBUB7     
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
+⭕Join Group: @BooksClubFranchise
